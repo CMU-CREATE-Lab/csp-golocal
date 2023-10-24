ab9bf9241a9e4d904f0499ee468edadf4a4c6cbc
mtasota <miketasota@gmail.com>
2023-10-24 13:20:40 -0400

    drop old businesses table/model

--- app/models/business.rb
+++
@@ -1,4 +0,0 @@
-class Business < ApplicationRecord
-  has_one_attached :logo
-  has_many_attached :gallery
-end
--- db/migrate/20230824180728_create_businesses.rb
+++
@@ -1,9 +0,0 @@
-class CreateBusinesses < ActiveRecord::Migration[7.0]
-  def change
-    create_table :businesses do |t|
-      t.string :name
-
-      t.timestamps
-    end
-  end
-end
