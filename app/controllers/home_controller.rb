35734604c4f3df2044cee977e7cd8abae7388c03
mtasota <miketasota@gmail.com>
2024-12-09 14:48:21 -0500

    [wip] 2024fall: header links and placeholder links

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,24 @@
 class HomeController < ApplicationController
 
-  def landing
+  layout "application_front"
+
+  def about_us
   end
 
-  def demo1
+  def about_partner_with_us
   end
 
-  def demo2
+  def about_catering_tips
   end
+
+  def about_our_partners
+  end
+
+  def about_events
+    # TODO this might be better served later as an index of a model type (events)
+  end
+
+  def about_testimonials
+  end
+
 end
