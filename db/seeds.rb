512a82fb70930fd5738c95bef4c1c2f26ee38626
mtasota <miketasota@gmail.com>
2025-02-24 13:35:21 -0500

    db seed (social media), business edit form tweaks

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,19 +6,20 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-Dir[Rails.root.join('db/seeds/*.rb')].sort.each do |file|
-  puts "Processing #{file.split('/').last}"
-  require file
-end
+## TODO create mechanism to check for seeding businesses? (defaults to create models and old TempBusiness, omit for now)
+#Dir[Rails.root.join('db/seeds/*.rb')].sort.each do |file|
+#  puts "Processing #{file.split('/').last}"
+#  require file
+#end
 
-Cuisine.create(name: "Asian")
-Cuisine.create(name: "Mexican")
+Cuisine.find_or_create_by(name: "Asian")
+Cuisine.find_or_create_by(name: "Mexican")
 
 # Social Media Sites
-facebook = SocialMediaSite.create(name: 'Facebook')
-twitter = SocialMediaSite.create(name: 'Twitter')
-instagram = SocialMediaSite.create(name: 'Instagram')
-website = SocialMediaSite.create(name: 'Website')
+facebook = SocialMediaSite.find_or_create_by(name: 'Facebook')
+twitter = SocialMediaSite.find_or_create_by(name: 'Twitter')
+instagram = SocialMediaSite.find_or_create_by(name: 'Instagram')
+website = SocialMediaSite.find_or_create_by(name: 'Website')
 # Attach logos
 # (taken from: https://icons8.com/icons/set/social-media)
 facebook.logo.attach(io: File.open(Rails.root.join('public', 'assets', 'social-media-sites', 'facebook-logo.png')), filename: 'facebook-logo.png')
