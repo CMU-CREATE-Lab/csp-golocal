de8f7f6e1ea7a478973d75d28a39a6d7480ad886
mtasota <miketasota@gmail.com>
2025-11-17 21:35:29 -0500

    [wip] '/admin/files' add http links

--- app/models/managed_file.rb
+++ app/models/managed_file.rb
@@ -4,4 +4,9 @@ class ManagedFile < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   validates :file, presence: true, size: { less_than: 200.megabytes }
 
+  def streamed_filename_path
+    #"/managed_files/stream/#{CGI.escape(self.name)}"
+    "/managed_files/stream/#{URI::DEFAULT_PARSER.escape(self.name)}"
+  end
+
 end
